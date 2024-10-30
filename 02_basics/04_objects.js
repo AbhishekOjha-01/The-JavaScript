ffe907c11339f4310c1c0fba73bcc48c44fc209d
AbhishekOjha-01 <157052227+AbhishekOjha-01@users.noreply.github.com>
2024-10-30 18:44:10 +0000

    objects in depth

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -29,3 +29,39 @@ console.log(regularUser.fullName.userFullName?.firstName)
 
 // ? ensures the availibility
 
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+const obj3 = {obj1, obj2}
+// a better way to concat
+
+// const obj4 = Object.assign(obj1, obj2)
+
+const obj4 = Object.assign({}, obj1, obj2)
+
+// {} --> used to reeturn all things in new object.
+
+console.log(obj3)
+console.log(obj4)
+
+// a most used syntax
+
+const obj5 = {...obj1, ...obj2}
+console.log(obj5)
+
+// a most used syntax in case of database
+
+console.log(Object.keys(instaUser))
+// returns the keys 
+
+console.log(Object.values(instaUser))
+// return the values
+
+console.log(Object.entries(instaUser))
+
+
+console.log(instaUser.hasOwnProperty(instaUser))
+
+/**
+ * 1. Study about objects.
+ */
